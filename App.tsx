4e57e6475a5c6ac293fb1244f308b4a85fe2cd54
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-11 20:02:48 +0200

    [feat] Use CSS helpers

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import theme from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { NewGroup } from '@screens/NewGroup';
 import { Groups } from '@screens/Groups';
 
 export default function App() {
@@ -19,7 +18,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <NewGroup/> : <Loading/> }
+        { fontsLoaded ? <Groups/> : <Loading/> }
       </>
     </ThemeProvider>
   );
