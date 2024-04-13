34cc5fc94d002610b1744c6d5a119ed1c6596a50
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-13 12:00:13 +0200

    [feat] Storage Players

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const GROUP_COLLECTION = '@gang-match-app:groups';
+const PLAYER_COLLECTION = '@gang-match-app:players';
 
-export { GROUP_COLLECTION }
+export { GROUP_COLLECTION, PLAYER_COLLECTION }
