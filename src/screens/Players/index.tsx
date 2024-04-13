b882d7c173027e1c8a9125080f3af7b9ee17d428
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-13 12:03:54 +0200

    [feat] Listing players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FlatList } from 'react-native'
+import { FlatList, Alert } from 'react-native'
 import { useRoute } from '@react-navigation/native';
 
 import { Header } from "@components/Header";
@@ -12,18 +12,47 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { AppError } from '@utils/AppError';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playersGetByGroup } from '@storage/player/playersGetByGroup';
 
 type RouteParams = {
   group: string;
 }
 
 export function Players() {
-  const [team, setTeam] = useState('Time A');
+  const [newPlayerName, setNewPlayerName] = useState('');
+  const [gang, setGang] = useState('Gang A');
   const [players, setPlayers] = useState([]);
 
-  const route = useRoute()
+  const route = useRoute();
 
-  const { group } = route.params as RouteParams
+  const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if(newPlayerName.trim().length === 0) {
+      return Alert.alert('New member', 'Write the name of the member you want to add.');
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      gang,
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const players = await playersGetByGroup(group);
+
+      console.log(players);
+    } catch (error) {
+      if(error instanceof AppError){
+        Alert.alert('New member', error.message);
+      } else {
+        console.log(error);
+        Alert.alert('New member', 'It was not possible to add them.')
+      }
+    }
+  }
 
   return (
     <Container>
@@ -31,17 +60,19 @@ export function Players() {
 
       <Highlight
         title={group}
-        subtitle="add the peeps and separate the teams"
+        subtitle="add a member and define the gang"
       />
 
       <Form>
         <Input
           placeholder="Name"
           autoCorrect={false}
+          onChangeText={setNewPlayerName}
         />
 
         <ButtonIcon
           icon="add"
+          onPress={handleAddPlayer}
         />
       </Form>
 
@@ -52,8 +83,8 @@ export function Players() {
           renderItem={({ item }) => (
             <Filter
               title={item}
-              isActive={item === team}
-              onPress={() => setTeam(item)}
+              isActive={item === gang}
+              onPress={() => setGang(item)}
             />
           )}
           horizontal
