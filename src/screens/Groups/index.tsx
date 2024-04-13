8c0940c4da9ef12749c5e459f340f37e94ac8747
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-13 11:49:23 +0200

    [feat] Get data from local storage on load

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,23 +1,40 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
+
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-
-import { Container } from './styles';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+
+import { Container } from './styles';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   function handleNewGroup() {
-    navigation.navigate('new')
+    navigation.navigate('new');
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll();
+      setGroups(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  },[]))
+
 
   return (
     <Container>
