ea8559ec268bcf0d8c42626a559d8bf12dcdba1f
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-13 11:53:55 +0200

    [feat] Handle with errors on creation

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
+import { AppError } from "@utils/AppError";
 import { groupCreate } from "@storage/group/groupCreate";
 
 import { Container, Content, Icon } from "./styles";
@@ -11,16 +13,26 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
-  const [group, setGroup] = useState('')
+  const [group, setGroup] = useState('');
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   async function handleNew() {
     try {
+
+      if(group.trim().length === 0) {
+        return Alert.alert('New gang', 'Write the gang name.');
+      }
+
       await groupCreate(group);
-      navigation.navigate('players', { group })
+      navigation.navigate('players', { group });
     } catch (error) {
-      console.log(error)
+      if(error instanceof AppError) {
+        Alert.alert('New gang', error.message);
+      } else {
+        Alert.alert('New gang', 'It was not possible to create the gang');
+        console.log(error);
+      }
     }
   }
 
